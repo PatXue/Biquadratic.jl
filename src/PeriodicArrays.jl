c292db7029737504bead46646f113cdb148974c1
PatXue <95881915+PatXue@users.noreply.github.com>
2025-07-11 13:58:29 -0400

    AbstractArray interface for PeriodicArray

--- src/PeriodicArrays.jl
+++ src/PeriodicArrays.jl
@@ -1,8 +1,16 @@
 module PeriodicArrays
 
+export PeriodicArray, PeriodicMatrix
+
 struct PeriodicArray{T, N} <: AbstractArray{T, N}
-    A::Array{T, N}
+    array::Array{T, N}
 end
 const PeriodicMatrix{T} = PeriodicArray{T, 2}
+
+Base.size(A::PeriodicArray) = size(A.array)
+function Base.getindex(A::PeriodicArray{T, N}, I::Vararg{Int, N}) where {T, N}
+    indices = map(mod1, I, size(A.array))
+    return getindex(A.array, indices...)
+end
 
 end
