31a34d898435f2cfb9fd23ba48ebc114969a5d67
PatXue <95881915+PatXue@users.noreply.github.com>
2025-07-13 16:51:10 -0400

    UnitVector struct

--- src/UnitVectors.jl
+++ src/UnitVectors.jl
@@ -1,3 +1,19 @@
 module UnitVectors
 
+using Random
+using StaticArrays
+using LinearAlgebra
+
+export UnitVector
+
+struct UnitVector <: FieldVector{3, Float64}
+    x::Float64
+    y::Float64
+    z::Float64
+    UnitVector(x, y, z) =
+        norm((x, y, z)) ≈ 1.0 ? new(x, y, z) : error("Non-unit unit vector")
+    UnitVector((x, y, z)::Tuple) =
+        norm((x, y, z)) ≈ 1.0 ? new(x, y, z) : error("Non-unit unit vector")
+end
+
 end
