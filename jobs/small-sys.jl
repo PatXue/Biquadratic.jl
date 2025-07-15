790c7da51af5298f7f70160961b58c2609cf9b4d
PatXue <95881915+PatXue@users.noreply.github.com>
2025-07-15 14:52:13 -0400

    Make small system sweep
    Testing thermalization time

---
+++ jobs/small-sys.jl
@@ -0,0 +1,35 @@
+import Pkg
+Pkg.activate("..")
+
+using Biquadratic
+using Carlo
+using Carlo.JobTools
+
+tm = TaskMaker()
+tm.rand_init = true
+
+L = 10
+tm.Lx = tm.Ly = L
+tm.sweeps = 0
+tm.thermalization = 20000
+tm.binsize = 100
+
+tm.T = 0.02
+tm.J2a = 1.0
+tm.J2b = -1.0
+Ks = (-0.2, 0.2)
+J1s = -2.0:0.1:2.0
+for K in Ks
+    tm.K = K
+    for J1 in J1s
+        tm.J1 = J1
+        task(tm)
+    end
+end
+
+job = JobInfo("temp-sweep", Biquadratic.MC;
+    run_time = "24:00:00",
+    checkpoint_time = "30:00",
+    tasks = make_tasks(tm),
+)
+start(job, ARGS)
